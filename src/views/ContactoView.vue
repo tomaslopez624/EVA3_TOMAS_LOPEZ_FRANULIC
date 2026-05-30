a260af4ee3c1c577d8dc383063e4e67a132bb599
tomaslopez624 <tomas.lopez@estudiante.ipss.cl>
2026-05-29 22:33:03 -0400

    feat: crear ContactoView.vue

---
+++ src/views/ContactoView.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <main class="contenedor">
+    <h1>Contáctanos</h1>
+    <p>Aquí construiremos el formulario de contacto para el Centro de Negocios.</p>
+  </main>
+</template>
+
+<style scoped>
+.contenedor {
+  padding: 2rem;
+  max-width: 1200px;
+  margin: 0 auto;
+  text-align: center;
+  font-family: Arial, sans-serif;
+}
+h1 {
+  color: #002b60;
+}
+</style>
